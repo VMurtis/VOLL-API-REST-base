1bad09088cf6237f0f84ceb5966a07f3ed5663d7
VMurtis <v.murtis@gmail.com>
2026-05-04 18:09:15 -0300

    implementa tratamento de erro 404 na API

--- api/api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,9 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.paciente.DadosCadastroPaciente;
-import med.voll.api.paciente.Paciente;
-import med.voll.api.paciente.PacienteRepository;
+import med.voll.api.domain.paciente.DadosCadastroPaciente;
+import med.voll.api.domain.paciente.Paciente;
+import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
